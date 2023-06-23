394c1fa9f93157aba2f6f4907d47aa8a63aa3d1a
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-06-23 23:12:35 +0000

    feat: create a separate folder with character interface

---
+++ src/interfaces/character.ts
@@ -0,0 +1,16 @@
+export interface Character {
+  id: number;
+  name: string;
+  description?: string;
+  thumbnail?: {
+    path?: string;
+    extension?: string;
+  };
+}
+
+export interface CharactersData {
+  data: {
+    results: Array<Character>;
+    total: number;
+  };
+}
--- src/repository/character.ts
+++ src/repository/character.ts
@@ -1,22 +1,6 @@
+import { CharactersData } from 'src/interfaces/character';
 import { marvelAPI } from '../../services/marvel';
 import { apiEndpoints } from './utils/apiEndpoints';
-
-interface Character {
-  id: number;
-  name: string;
-  description?: string;
-  thumbnail?: {
-    path?: string;
-    extension?: string;
-  };
-}
-
-interface CharactersData {
-  data: {
-    results: Array<Character>;
-    total: number;
-  };
-}
 
 async function getCharacters(offset: number): Promise<CharactersData> {
   const data = await marvelAPI<CharactersData>(
