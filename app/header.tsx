d7f61f48a664dd5983e91a976d3336646208abe8
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-06-27 21:42:50 +0000

    feat(styles): add margin bottom to header

--- app/header.tsx
+++ app/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="mt-3 flex flex-col justify-center items-center">
+    <header className="mt-3 mb-5 flex flex-col justify-center items-center">
       <div className=" w-40 bg-white m-auto flex justify-center items-center rounded-sm">
         <h1 className="text-5xl text-center text-red-3 font-bangers px-1 uppercase">
           Marvel
