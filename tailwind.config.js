5102142a8cde62785632b71df93d410ee9452ffe
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-06-27 00:39:43 +0000

    chore: tailwind configs and extend colors

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./app/**/*.{jsx,ts,tsx,mdx}', './src/**/*.{jsx,ts,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        red: {
+          0: '#770606',
+          1: '#A00707',
+          2: '#BA0707',
+          3: '#D6231F',
+        },
+        blue: {
+          0: '#453A85',
+          1: '#417FD4',
+          2: '#478FE1',
+          3: '#7389E5',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
