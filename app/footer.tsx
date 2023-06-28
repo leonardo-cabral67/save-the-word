f4dfdf38e22f05637e31b355b6e64a0908efcf52
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-06-28 01:06:15 +0000

    feat: create `footer`component

---
+++ app/footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer
+      className="w-full border-t
+      mt-12 border-white mx-auto shrink-0 py-4"
+    >
+      <h4 className="text-white text-sm text-center font-light mx-auto">
+        Developed by{' '}
+        <a
+          className="underline"
+          href="https://github.com/leonardo-cabral67"
+          rel="noreferrer"
+        >
+          Leonardo Cabral
+        </a>
+      </h4>
+    </footer>
+  );
+}
