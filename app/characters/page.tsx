462e6cf90bfcf25f58c52ae1fcd01aacb7a021b8
Leonardo Cabral <leonardolccabral@gmail.com>
2023-06-28 21:52:03 +0000

    feat: implement filterCharacters function

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState, useRef } from 'react';
 import CharacterCard from './characterCard';
 import ToggleOption from './toggleOption';
 import Input from './input';
+import CharactersSkeletonLoader from './charactersSkeletonLoader';
 
 interface Character {
   id: number;
@@ -18,7 +19,14 @@ interface Character {
 export default function CharacterPage() {
   const completeInitialLoad = useRef(false);
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [filter, setFilter] = useState<string>('');
   const [onFilter, setOnFilter] = useState<boolean>(true);
+  const isLoading = characters.length === 0;
+
+  const filteredCharacters = charactersController.filterCharacters<Character>({
+    filterSearch: filter,
+    characters: characters,
+  });
 
   function toggleOnFilter() {
     setOnFilter(!onFilter);
@@ -58,14 +66,40 @@ export default function CharacterPage() {
           />
         </div>
         {onFilter ? (
-          <Input placeholder="Filter character by name" />
+          <Input
+            placeholder="Filter character by name"
+            onChange={(e) => setFilter(e.target.value)}
+          />
         ) : (
           <Input placeholder="Search character by name" />
         )}
       </section>
 
+      {filter.length > 0 && (
+        <div className="flex flex-col items-center -mt-24">
+          <h3
+            className="mb-12 text-center text-3xl text-white font-semibold
+        drop-shadow-choose-character font-bangers -tracking-tighter"
+          >
+            Current filter: {filter}
+          </h3>
+          <span
+            className="text-lg text-white font-semibold -mt-3 mb-4
+             font-bangers -tracking-tighter"
+          >
+            Total results: {` ${filteredCharacters.length}`}
+          </span>
+        </div>
+      )}
+
+      {isLoading && (
+        <ul className="grid grid-cols-fluid gap-6 justify-items-center">
+          <CharactersSkeletonLoader cards={20} />
+        </ul>
+      )}
+
       <ul className="grid grid-cols-fluid gap-6 justify-items-center">
-        {characters.map((character) => (
+        {filteredCharacters.map((character) => (
           <CharacterCard key={character.id} character={character} />
         ))}
       </ul>
