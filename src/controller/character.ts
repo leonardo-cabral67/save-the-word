976c28522324356a8ba5093b1981602edf04f768
Leonardo Cabral <leonardolccabral@gmail.com>
2023-06-29 02:57:23 +0000

    feat: `searchCharactersByName` controller function

--- src/controller/character.ts
+++ src/controller/character.ts
@@ -1,10 +1,19 @@
-// import { charactersRepository } from '@repository/character';
 import { CharactersData } from 'src/interfaces/character';
 import { charactersRepository } from '../repository/character';
 
 async function getCharacters(page: number): Promise<CharactersData> {
   const pageNumber = page === 1 ? 0 : 20 * (page - 1);
   const response = await charactersRepository.getCharacters(pageNumber);
+  return response;
+}
+
+async function searchCharactersByName(search: string): Promise<CharactersData> {
+  const nameStartsWith: string = encodeURIComponent(
+    search.toLowerCase().trim()
+  );
+  const response = await charactersRepository.getCharacterByName(
+    nameStartsWith
+  );
   return response;
 }
 
@@ -26,4 +35,5 @@ function filterCharacters<TypeCharacter>({
 export const charactersController = {
   getCharacters,
   filterCharacters,
+  searchCharactersByName,
 };
