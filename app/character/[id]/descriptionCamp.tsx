a7f98106119485ee87e746a83c034c5376457f6c
Leonardo Cabral <leonardolccabral@gmail.com>
2023-07-01 15:06:08 +0000

    feat(styles): add text-center to `DescriptionCamp`

--- app/character/[id]/descriptionCamp.tsx
+++ app/character/[id]/descriptionCamp.tsx
@@ -8,7 +8,7 @@ export default function DescriptionCamp({
   description,
 }: DescriptionCampProps) {
   return (
-    <div className="w-full flex flex-col items-center justify-around">
+    <div className="w-full flex flex-col items-center justify-around text-center">
       <h2 className="text-4xl text-white font-bangers">{title}:</h2>
       <p className="text-sm text-white">{description}</p>
     </div>
